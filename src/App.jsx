682f3aa6cd6c67478d40ed3b78987a7ecb77c172
Atiar Osman <atiar1103@gmail.com>
2026-04-07 20:57:40 +0600

    Switched from BrowserRouter to HashRouter to absolutely prevent Github Pages 404 router crashes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import QuizHome from './pages/Quiz/QuizHome';
 import QuizEngine from './pages/Quiz/QuizEngine';
